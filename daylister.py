efe6909c2d6ad59647b8f107cc0a5fd595303a53
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-02 11:08:16 -0300

    Update daylister.py

--- daylister.py
+++ daylister.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 # Solicita ao usuário o ano mínimo e máximo
 ano_minimo = int(input("Digite o ano mínimo: "))
@@ -10,14 +10,18 @@ nome_arquivo_saida = input("Digite o nome do arquivo de saída (ex: datas.txt): 
 # Lista para armazenar as datas
 datas = []
 
-# Loop para gerar as datas
+# Loop para gerar todas as combinações de dia e mês entre os anos
 for ano in range(ano_minimo, ano_maximo + 1):
-    # Formato com barra (dd/mm/aaaa)
-    data_barra = date(ano, 1, 1).strftime("%d/%m/%Y")
-    # Formato sem barra (ddmmyyyy)
-    data_sem_barra = date(ano, 1, 1).strftime("%d%m%Y")
-    datas.append(data_barra)
-    datas.append(data_sem_barra)
+    for mes in range(1, 13):
+        ultimo_dia_mes = (date(ano, mes % 12 + 1, 1) - timedelta(days=1)).day
+        for dia in range(1, ultimo_dia_mes + 1):
+            data = date(ano, mes, dia)
+            # Formato com barra (dd/mm/aaaa)
+            data_barra = data.strftime("%d/%m/%Y")
+            # Formato sem barra (ddmmyyyy)
+            data_sem_barra = data.strftime("%d%m%Y")
+            datas.append(data_barra)
+            datas.append(data_sem_barra)
 
 # Escreve a lista de datas no arquivo de saída
 with open(nome_arquivo_saida, 'w') as arquivo_saida:
