9e19c1e2151243f711d26ab762d0ef785c18d886
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-02 11:03:27 -0300

    Create daylister.py

---
+++ daylister.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+# Solicita ao usuário o ano mínimo e máximo
+ano_minimo = int(input("Digite o ano mínimo: "))
+ano_maximo = int(input("Digite o ano máximo: "))
+
+# Solicita ao usuário o nome do arquivo de saída
+nome_arquivo_saida = input("Digite o nome do arquivo de saída (ex: datas.txt): ")
+
+# Lista para armazenar as datas
+datas = []
+
+# Loop para gerar as datas
+for ano in range(ano_minimo, ano_maximo + 1):
+    # Formato com barra (dd/mm/aaaa)
+    data_barra = date(ano, 1, 1).strftime("%d/%m/%Y")
+    # Formato sem barra (ddmmyyyy)
+    data_sem_barra = date(ano, 1, 1).strftime("%d%m%Y")
+    datas.append(data_barra)
+    datas.append(data_sem_barra)
+
+# Escreve a lista de datas no arquivo de saída
+with open(nome_arquivo_saida, 'w') as arquivo_saida:
+    for data in datas:
+        arquivo_saida.write(data + '\n')
+
+print(f"As datas foram salvas no arquivo '{nome_arquivo_saida}'.")
